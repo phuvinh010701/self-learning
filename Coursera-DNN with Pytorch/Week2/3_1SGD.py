0b3a9a2334390cd4961af92dd8c640f85a56118c
Nguyễn Phú Vinh <nguyenphuvinh01@gmail.com>
2022-08-25 16:08:46 +0700

    Create 3_1SGD.py

---
+++ Coursera-DNN with Pytorch/Week2/3_1SGD.py
@@ -0,0 +1,79 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+
+class Data(Dataset):
+    
+    # Constructor
+    def __init__(self):
+        self.x = torch.arange(-3, 3, 0.1).view(-1, 1)
+        self.y = 1 * self.x - 1
+        self.len = self.x.shape[0]
+        
+    # Getter
+    def __getitem__(self,index):    
+        return self.x[index], self.y[index]
+    
+    # Return the length
+    def __len__(self):
+        return self.len
+
+dataset = Data()
+print("The length of dataset: ", len(dataset))
+
+
+trainloader = DataLoader(dataset = dataset, batch_size = 1)
+
+# The function for training the model
+X = torch.arange(-3, 3, 0.1).view(-1, 1)
+f = 1 * X - 1
+Y = f + 0.1 * torch.randn(X.size())
+w = torch.tensor(-15.0,requires_grad=True)
+b = torch.tensor(-10.0,requires_grad=True)
+LOSS_Loader = []
+lr = 0.001
+
+def forward(X):
+    return w * X + b
+
+def criterion(yhat, y):
+    return torch.mean((yhat - y) ** 2)
+def train_model_DataLoader(epochs):
+    
+    # Loop
+    for epoch in range(epochs):
+        
+        # SGD is an approximation of out true total loss/cost, in this line of code we calculate our true loss/cost and store it
+        Yhat = forward(X)
+        
+        # store the loss 
+        LOSS_Loader.append(criterion(Yhat, Y).tolist())
+        
+        for x, y in trainloader:
+            
+            # make a prediction
+            yhat = forward(x)
+            
+            # calculate the loss
+            loss = criterion(yhat, y)
+            
+            # Section for plotting
+            # get_surface.set_para_loss(w.data.tolist(), b.data.tolist(), loss.tolist())
+            
+            # Backward pass: compute gradient of the loss with respect to all the learnable parameters
+            loss.backward()
+            
+            # Updata parameters slope
+            w.data = w.data - lr * w.grad.data
+            b.data = b.data - lr* b.grad.data
+            
+            # Clear gradients 
+            w.grad.data.zero_()
+            b.grad.data.zero_()
+            
+        #plot surface and data space after each epoch    
+        # get_surface.plot_ps()
+
+
+train_model_DataLoader(20)
+print(LOSS_Loader)
