725fbd859bdf140c01dbd402e000eebd4a3b50ac
Nguyễn Phú Vinh <nguyenphuvinh01@gmail.com>
2022-08-28 15:49:10 +0700

    Update pipeline_train_model_withPytorch.py

--- Coursera-DNN with Pytorch/pipeline_train_model_withPytorch.py
+++ Coursera-DNN with Pytorch/pipeline_train_model_withPytorch.py
@@ -1,5 +1,15 @@
+from torch import nn, optim
+from torch.utils.data import Dataset, DataLoader
+
+
+# example linear regression
+model = nn.Linear(2, 1)
+criterion = nn.MSELoss()
+optimizer = optim.SGD(model.parameters(), lr=0.1)
+
+train_loader = DataLoader(dataset=data_set, batch_size=2)
 for epoch in range(epochs):
-    for x, y in trainloader:
+    for x, y in train_loader:
         yhat = model(x) # forward
         loss = criterion(yhat, y) # calculate loss
         optimizer.zero_grad() # set gradient to zero
